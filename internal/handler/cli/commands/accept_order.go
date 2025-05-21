1e499b2e62fd1568d9fd8234636a0a1a638ceeed
Egor Ponyaev <epchamp001@gmail.com>
2025-05-21 11:19:08 +0300

    [GOJHW-1] The order acceptance case has been fixed: ORDER_ACCEPTED: --user-id. Removed the automatic cmd.Mark Flag Required. Set Silence Usage commands in the root so that Cobra does not print Usage for any error flags. In the RunE itself, I check both the flags and their values.

--- internal/handler/cli/commands/accept_order.go
+++ internal/handler/cli/commands/accept_order.go
@@ -5,17 +5,30 @@ import (
 	"github.com/spf13/cobra"
 	"pvz-cli/internal/domain/codes"
 	"pvz-cli/internal/usecase"
+	"strings"
 	"time"
 )
 
 func NewAcceptOrderCmd(svc usecase.Service) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "accept-order",
-		Short: "Принять заказ от курьера",
+		Use:           "accept-order",
+		Short:         "Принять заказ от курьера",
+		SilenceUsage:  true,
+		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			orderID, _ := cmd.Flags().GetString("order-id")
 			userID, _ := cmd.Flags().GetString("user-id")
 			expStr, _ := cmd.Flags().GetString("expires")
+
+			// валидация на корректные данные
+			if orderID == "" || strings.HasPrefix(orderID, "-") {
+				printErr(codes.ErrValidationFailed)
+				return nil
+			}
+			if userID == "" || strings.HasPrefix(userID, "-") {
+				printErr(codes.ErrValidationFailed)
+				return nil
+			}
 
 			exp, err := time.Parse("2006-01-02", expStr)
 			if err != nil {
@@ -36,9 +49,6 @@ func NewAcceptOrderCmd(svc usecase.Service) *cobra.Command {
 	cmd.Flags().String("order-id", "", "ID заказа (обязательно)")
 	cmd.Flags().String("user-id", "", "ID клиента (обязательно)")
 	cmd.Flags().String("expires", "", "Срок хранения YYYY-MM-DD")
-	_ = cmd.MarkFlagRequired("order-id")
-	_ = cmd.MarkFlagRequired("user-id")
-	_ = cmd.MarkFlagRequired("expires")
 
 	return cmd
 }
